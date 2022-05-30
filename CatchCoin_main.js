dd06ccf7a3d4c52c9a9bedab08f84966ccc5655f
anb389303 <78268075+anb389303@users.noreply.github.com>
2022-05-30 18:03:31 -0400

    Update CatchCoin_main.js

--- CatchCoin_main.js
+++ CatchCoin_main.js
@@ -226,11 +226,11 @@
         // execute only if !gameOver
         if (gameOver) return;
 
-        if (game.input.pointer1.isDown){ if (Math.floor(game.input.x/(game.width/2)= LEFT){ knight.body.velocity.x = 150;     
-                                                                                                knight.animations.play('right');    }  
-                                        if (Math.floor(game.input.x/(game.width/2)=RIGHT) { knight.body.velocity.x = -150;      
-                                                                                                knight.animations.play('left');    }    
-        }else{ knight.animations.stop();      knight.frame = 4;  }
+        //if (game.input.pointer1.isDown){ if (Math.floor(game.input.x/(game.width/2)= LEFT){ knight.body.velocity.x = 150;     
+                                                                                              //  knight.animations.play('right');    }  
+                                       // if (Math.floor(game.input.x/(game.width/2)=RIGHT) { knight.body.velocity.x = -150;      
+                                                                                               // knight.animations.play('left');    }    
+       // }else{ knight.animations.stop();      knight.frame = 4;  }
             
                   
             //add move keypad funcitionality
